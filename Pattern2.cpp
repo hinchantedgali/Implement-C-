689c6704ce14febf12d21f95d4d3372917068e19
Hina Chandra <hinchanted505@gmail.com>
2023-07-05 13:48:11 +0530

    added some new files.

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void pattern1(int n)
+void pattern2(int n)
 {
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j <= i; j++)
         {
             cout << "*";
         }
@@ -23,7 +23,7 @@ int main()
 
     int n;
     cin >> n;
-    pattern1(n);
+    pattern2(n);
 
     return 0;
 }
