a7c123fcb598a310ea06deeb3285f06422b0fc6d
hinchantedgali <138198025+hinchantedgali@users.noreply.github.com>
2023-08-29 18:00:07 +0530

    Update Pattern7.cpp

--- Pattern7.cpp
+++ Pattern7.cpp
@@ -3,22 +3,20 @@ using namespace std;
 
 void pattern7(int n)
 {
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i; j++)
+    for (int i = 0; i < n; i++)
         {
-            cout << " ";
+            for (int j = 1; j < n - i; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 0; j < (2 * i + 1); j++)
+            {
+                cout << "*";
+            }
+            
+            cout << endl;
+            
         }
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            cout << "*";
-        }
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << " ";
-        }
-        cout << endl;
-    }
 }
 
 int main()
